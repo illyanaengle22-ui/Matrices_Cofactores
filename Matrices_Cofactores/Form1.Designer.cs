a9fdc23abfe9eb1508c301d8899ea47b1284b47d
ill-707 <illyana.engle22@unach.mx>
2025-09-14 13:03:08 -0600

    Agregar archivos de proyecto.

---
+++ Matrices_Cofactores/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Matrices_Cofactores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rd2x2 = new System.Windows.Forms.RadioButton();
+            this.rd3x3 = new System.Windows.Forms.RadioButton();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.GP2x2 = new System.Windows.Forms.GroupBox();
+            this.txtA22 = new System.Windows.Forms.TextBox();
+            this.txtA21 = new System.Windows.Forms.TextBox();
+            this.txtA12 = new System.Windows.Forms.TextBox();
+            this.txtA11 = new System.Windows.Forms.TextBox();
+            this.GB3x3 = new System.Windows.Forms.GroupBox();
+            this.txtA33 = new System.Windows.Forms.TextBox();
+            this.txtA32 = new System.Windows.Forms.TextBox();
+            this.txtA31 = new System.Windows.Forms.TextBox();
+            this.txtA23 = new System.Windows.Forms.TextBox();
+            this.txtA13 = new System.Windows.Forms.TextBox();
+            this.txtA22_3x3 = new System.Windows.Forms.TextBox();
+            this.txtA21_3x3 = new System.Windows.Forms.TextBox();
+            this.txtA12_3x3 = new System.Windows.Forms.TextBox();
+            this.txtA11_3x3 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnCeros = new System.Windows.Forms.Button();
+            this.lblDet = new System.Windows.Forms.Label();
+            this.GP2x2.SuspendLayout();
+            this.GB3x3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rd2x2
+            // 
+            this.rd2x2.AutoSize = true;
+            this.rd2x2.Location = new System.Drawing.Point(210, 115);
+            this.rd2x2.Name = "rd2x2";
+            this.rd2x2.Size = new System.Drawing.Size(86, 20);
+            this.rd2x2.TabIndex = 0;
+            this.rd2x2.Text = "Matriz 2x2";
+            this.rd2x2.UseVisualStyleBackColor = true;
+            this.rd2x2.CheckedChanged += new System.EventHandler(this.rd2x2_CheckedChanged);
+            // 
+            // rd3x3
+            // 
+            this.rd3x3.AutoSize = true;
+            this.rd3x3.Location = new System.Drawing.Point(425, 115);
+            this.rd3x3.Name = "rd3x3";
+            this.rd3x3.Size = new System.Drawing.Size(86, 20);
+            this.rd3x3.TabIndex = 1;
+            this.rd3x3.Text = "Matriz 3x3";
+            this.rd3x3.UseVisualStyleBackColor = true;
+            this.rd3x3.CheckedChanged += new System.EventHandler(this.rd3x3_CheckedChanged);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(320, 34);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(131, 16);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Matriz por cofactores";
+            // 
+            // GP2x2
+            // 
+            this.GP2x2.Controls.Add(this.lblDet);
+            this.GP2x2.Controls.Add(this.txtA22);
+            this.GP2x2.Controls.Add(this.txtA21);
+            this.GP2x2.Controls.Add(this.txtA12);
+            this.GP2x2.Controls.Add(this.txtA11);
+            this.GP2x2.Location = new System.Drawing.Point(172, 174);
+            this.GP2x2.Name = "GP2x2";
+            this.GP2x2.Size = new System.Drawing.Size(424, 232);
+            this.GP2x2.TabIndex = 3;
+            this.GP2x2.TabStop = false;
+            this.GP2x2.Text = "Matriz 2x2";
+            // 
+            // txtA22
+            // 
+            this.txtA22.Location = new System.Drawing.Point(218, 114);
+            this.txtA22.Name = "txtA22";
+            this.txtA22.Size = new System.Drawing.Size(65, 22);
+            this.txtA22.TabIndex = 3;
+            // 
+            // txtA21
+            // 
+            this.txtA21.Location = new System.Drawing.Point(126, 114);
+            this.txtA21.Name = "txtA21";
+            this.txtA21.Size = new System.Drawing.Size(65, 22);
+            this.txtA21.TabIndex = 2;
+            // 
+            // txtA12
+            // 
+            this.txtA12.Location = new System.Drawing.Point(218, 64);
+            this.txtA12.Name = "txtA12";
+            this.txtA12.Size = new System.Drawing.Size(65, 22);
+            this.txtA12.TabIndex = 1;
+            // 
+            // txtA11
+            // 
+            this.txtA11.Location = new System.Drawing.Point(126, 64);
+            this.txtA11.Name = "txtA11";
+            this.txtA11.Size = new System.Drawing.Size(65, 22);
+            this.txtA11.TabIndex = 0;
+            // 
+            // GB3x3
+            // 
+            this.GB3x3.Controls.Add(this.txtA33);
+            this.GB3x3.Controls.Add(this.txtA32);
+            this.GB3x3.Controls.Add(this.txtA31);
+            this.GB3x3.Controls.Add(this.txtA23);
+            this.GB3x3.Controls.Add(this.txtA13);
+            this.GB3x3.Controls.Add(this.txtA22_3x3);
+            this.GB3x3.Controls.Add(this.txtA21_3x3);
+            this.GB3x3.Controls.Add(this.txtA12_3x3);
+            this.GB3x3.Controls.Add(this.txtA11_3x3);
+            this.GB3x3.Location = new System.Drawing.Point(172, 174);
+            this.GB3x3.Name = "GB3x3";
+            this.GB3x3.Size = new System.Drawing.Size(424, 232);
+            this.GB3x3.TabIndex = 4;
+            this.GB3x3.TabStop = false;
+            this.GB3x3.Text = "Matriz 3x3";
+            // 
+            // txtA33
+            // 
+            this.txtA33.Location = new System.Drawing.Point(291, 145);
+            this.txtA33.Name = "txtA33";
+            this.txtA33.Size = new System.Drawing.Size(65, 22);
+            this.txtA33.TabIndex = 8;
+            // 
+            // txtA32
+            // 
+            this.txtA32.Location = new System.Drawing.Point(197, 145);
+            this.txtA32.Name = "txtA32";
+            this.txtA32.Size = new System.Drawing.Size(65, 22);
+            this.txtA32.TabIndex = 7;
+            // 
+            // txtA31
+            // 
+            this.txtA31.Location = new System.Drawing.Point(105, 145);
+            this.txtA31.Name = "txtA31";
+            this.txtA31.Size = new System.Drawing.Size(65, 22);
+            this.txtA31.TabIndex = 6;
+            // 
+            // txtA23
+            // 
+            this.txtA23.Location = new System.Drawing.Point(291, 94);
+            this.txtA23.Name = "txtA23";
+            this.txtA23.Size = new System.Drawing.Size(65, 22);
+            this.txtA23.TabIndex = 5;
+            // 
+            // txtA13
+            // 
+            this.txtA13.Location = new System.Drawing.Point(291, 44);
+            this.txtA13.Name = "txtA13";
+            this.txtA13.Size = new System.Drawing.Size(65, 22);
+            this.txtA13.TabIndex = 4;
+            // 
+            // txtA22_3x3
+            // 
+            this.txtA22_3x3.Location = new System.Drawing.Point(197, 94);
+            this.txtA22_3x3.Name = "txtA22_3x3";
+            this.txtA22_3x3.Size = new System.Drawing.Size(65, 22);
+            this.txtA22_3x3.TabIndex = 3;
+            // 
+            // txtA21_3x3
+            // 
+            this.txtA21_3x3.Location = new System.Drawing.Point(105, 94);
+            this.txtA21_3x3.Name = "txtA21_3x3";
+            this.txtA21_3x3.Size = new System.Drawing.Size(65, 22);
+            this.txtA21_3x3.TabIndex = 2;
+            // 
+            // txtA12_3x3
+            // 
+            this.txtA12_3x3.Location = new System.Drawing.Point(197, 44);
+            this.txtA12_3x3.Name = "txtA12_3x3";
+            this.txtA12_3x3.Size = new System.Drawing.Size(65, 22);
+            this.txtA12_3x3.TabIndex = 1;
+            // 
+            // txtA11_3x3
+            // 
+            this.txtA11_3x3.Location = new System.Drawing.Point(105, 44);
+            this.txtA11_3x3.Name = "txtA11_3x3";
+            this.txtA11_3x3.Size = new System.Drawing.Size(65, 22);
+            this.txtA11_3x3.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(253, 412);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(359, 412);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 5;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnCeros
+            // 
+            this.btnCeros.Location = new System.Drawing.Point(473, 412);
+            this.btnCeros.Name = "btnCeros";
+            this.btnCeros.Size = new System.Drawing.Size(152, 23);
+            this.btnCeros.TabIndex = 6;
+            this.btnCeros.Text = "Rellenar con ceros";
+            this.btnCeros.UseVisualStyleBackColor = true;
+            this.btnCeros.Click += new System.EventHandler(this.btnCeros_Click);
+            // 
+            // lblDet
+            // 
+            this.lblDet.AutoSize = true;
+            this.lblDet.Location = new System.Drawing.Point(148, 163);
+            this.lblDet.Name = "lblDet";
+            this.lblDet.Size = new System.Drawing.Size(131, 16);
+            this.lblDet.TabIndex = 7;
+            this.lblDet.Text = "Matriz por cofactores";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GB3x3);
+            this.Controls.Add(this.btnCeros);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.GP2x2);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.rd3x3);
+            this.Controls.Add(this.rd2x2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.GP2x2.ResumeLayout(false);
+            this.GP2x2.PerformLayout();
+            this.GB3x3.ResumeLayout(false);
+            this.GB3x3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rd2x2;
+        private System.Windows.Forms.RadioButton rd3x3;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.GroupBox GP2x2;
+        private System.Windows.Forms.GroupBox GB3x3;
+        private System.Windows.Forms.TextBox txtA33;
+        private System.Windows.Forms.TextBox txtA32;
+        private System.Windows.Forms.TextBox txtA31;
+        private System.Windows.Forms.TextBox txtA23;
+        private System.Windows.Forms.TextBox txtA13;
+        private System.Windows.Forms.TextBox txtA22_3x3;
+        private System.Windows.Forms.TextBox txtA21_3x3;
+        private System.Windows.Forms.TextBox txtA12_3x3;
+        private System.Windows.Forms.TextBox txtA11_3x3;
+        private System.Windows.Forms.TextBox txtA22;
+        private System.Windows.Forms.TextBox txtA21;
+        private System.Windows.Forms.TextBox txtA12;
+        private System.Windows.Forms.TextBox txtA11;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnCeros;
+        private System.Windows.Forms.Label lblDet;
+    }
+}
+
---
+++ Matrices_Cofactores/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matrices_Cofactores
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+         
+            GP2x2.Hide();
+            GB3x3.Hide();
+            rd2x2.Checked = false;
+            rd3x3.Checked = false;
+
+        }
+        private void rd2x2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rd2x2.Checked)
+            {
+                GP2x2.Show();
+                GB3x3.Hide(); 
+            }
+        }
+
+        private void rd3x3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rd3x3.Checked)
+            {
+                GB3x3.Show(); 
+                GP2x2.Hide();
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (rd2x2.Checked)
+            {
+                txtA11.Clear();
+                txtA12.Clear();
+                txtA21.Clear();
+                txtA22.Clear();
+            }
+            else if (rd3x3.Checked)
+            {
+                txtA11_3x3.Clear();
+                txtA12_3x3.Clear();
+                txtA13.Clear();
+                txtA21_3x3.Clear();
+                txtA22_3x3.Clear ();
+                txtA23.Clear();
+                txtA31.Clear();
+                txtA32.Clear();
+                txtA33.Clear();
+            }
+           
+        }
+
+        private void btnCeros_Click(object sender, EventArgs e)
+        {
+            if (rd2x2.Checked)
+            {
+                LlenarCeros(txtA11, e);
+                LlenarCeros(txtA12, e);
+                LlenarCeros(txtA21, e);
+                LlenarCeros(txtA22, e);
+            }
+            else if (rd3x3.Checked)
+            {
+                LlenarCeros(txtA11_3x3, e);
+                LlenarCeros(txtA12_3x3, e);
+                LlenarCeros(txtA13, e);
+                LlenarCeros(txtA21_3x3, e);
+                LlenarCeros(txtA22_3x3, e);
+                LlenarCeros(txtA23, e);
+                LlenarCeros(txtA31, e);
+                LlenarCeros(txtA32, e);
+                LlenarCeros(txtA33, e);
+            }
+
+        
+
+        }
+        private void LlenarCeros (object sender, EventArgs e)
+        {
+         
+            TextBox txtbox = (TextBox)sender;
+            if (string.IsNullOrEmpty(txtbox.Text))
+            {
+                txtbox.Text = "0";
+            }
+        }
+        private static double CalcularDet2x2(double[][] m)
+        {
+            return
+                (m[0][0] * m[1][1]
+                - m[0][1] * m[1][0]);
+            
+        }
+        private static double CalcularDet3x3(double[][] m)
+        {
+
+            var a = m[0][0];
+            var b = m[0][1];
+            var c = m[0][2];
+
+
+            var efhi = new double[][]
+            {
+                new double [] {m[1][1], m[1][2] },
+                new double [] { m[2][1], m[2][2] }
+            };
+               var dfgi = new double[][]
+            {
+                new double [] {m[1][0], m[1][2] },
+                new double [] { m[2][0], m[2][2] }
+            };
+               var degh = new double[][]
+            {
+                new double [] {m[1][0], m[1][1] },
+                new double [] { m[2][0], m[2][1] }
+            };
+            double det = (
+                a * CalcularDet2x2(efhi)
+                -b * CalcularDet2x2(dfgi)
+                +c * CalcularDet2x2(degh));
+            return det;
+
+        }
+        private void Det2x2()
+        {
+            double[][] m = new double[2][];
+            m[0] = new double[2];
+            m[1] = new double[2];
+
+            m[0][0] = double.Parse(txtA11.Text);
+            m[0][1] = double.Parse(txtA12.Text);
+            m[1][0] = double.Parse(txtA21.Text);
+            m[1][1] = double.Parse(txtA22.Text);
+
+            double det = CalcularDet2x2(m);
+            if (det != 0)
+            {
+                lblDet.Text = det.ToString();
+                MessageBox.Show("Determinante = " + det.ToString()); 
+            }
+            else
+            {
+                MessageBox.Show("No tiene solucion Determinante = " + det.ToString());
+            }
+        }
+        private void Det3x3()
+        {
+            double[][] m = new double[3][];
+            m[0] = new double[3];
+            m[1] = new double[3];
+            m[2] = new double[3];
+
+            m[0][0] = double.Parse(txtA11_3x3.Text);
+            m[0][1] = double.Parse(txtA12_3x3.Text);
+            m[0][2] = double.Parse(txtA13.Text);
+            m[1][0] = double.Parse(txtA21_3x3.Text);
+            m[1][1] = double.Parse(txtA22_3x3.Text);
+            m[1][2] = double.Parse(txtA23.Text);
+            m[2][0] = double.Parse(txtA31.Text);
+            m[2][1] = double.Parse(txtA32.Text);
+            m[2][2] = double.Parse(txtA33.Text);
+
+            double det = CalcularDet3x3(m);
+            if (det != 0)
+            {
+                MessageBox.Show("Determinante = " + det.ToString());
+            }
+            else
+            {
+                MessageBox.Show("No tiene solucion Determinante = " + det.ToString());
+            }
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (rd2x2.Checked && GP2x2.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))
+            {
+                MessageBox.Show("Ingrese todos los datos del 2x2");
+                return;
+            }
+            else if (rd3x3.Checked && GB3x3.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))
+            {
+                MessageBox.Show("Ingrese todos los datos del 3x3");
+                return;
+            }
+
+            else
+            {
+                if (rd2x2.Checked)
+                {
+                    var ms = MessageBox.Show("Antes de continuar compruebe dos veces", "Desea Continuar?", MessageBoxButtons.YesNo);
+                    if(ms == DialogResult.Yes)
+                    {
+                        Det2x2();
+                     
+                    }
+
+                }
+                if (rd3x3.Checked)
+                {
+                    Det3x3();
+                } 
+            }
+        }
+    }
+}
